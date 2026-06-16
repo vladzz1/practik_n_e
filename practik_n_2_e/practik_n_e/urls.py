5fa6e2497494f8e9070136b685a77ea8625515c0
Admin <Admin@DESKTOP-B6M267U>
2026-06-16 22:01:39 +0300

    add file categories.html

--- practik_n_2_e/practik_n_e/urls.py
+++ practik_n_2_e/practik_n_e/urls.py
@@ -26,6 +26,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include("users.urls")),
+    path('categories/', views.categories, name='categories'),
     path('', views.homepage, name='homepage')
 ]
 
